ed70c97c167eae0e9f9bb9b05ae17096781a770d
Yolaine Courteau <yolaine.courteau@videotron.ca>
2022-07-20 18:35:04 -0400

    eslint modif a la config de crlf package remove install de cucumber  et preprocessor

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,22 +2,28 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+    "@vue/eslint-config-prettier",
   ],
-  "overrides": [
+  overrides: [
     {
-      "files": [
+      files: [
         "**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
-        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"
+        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}",
       ],
-      "extends": [
-        "plugin:cypress/recommended"
-      ]
-    }
+      extends: ["plugin:cypress/recommended"],
+    },
+  ],
+  rules:[
+    "prettier/prettier" [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ],
   ]
-}
+};
